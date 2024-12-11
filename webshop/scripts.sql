acb118358bbaf3f32ebc5ffb5f92b660746b51bf
Mario Sutalo <mario.sutalo@gmail.com>
2024-12-11 10:57:29 +0100

    -cp

--- webshop/scripts.sql
+++ webshop/scripts.sql
@@ -1,8 +1,9 @@
+use webshop
+
 create table categories (
 id int primary key auto_increment,
 name varchar(50)
 );
-
 
 create table products (
 id int primary key auto_increment,
@@ -17,4 +18,11 @@ insert into categories (name)
 values 	("laptops"),
 		("monitors"),
 		("keyboards");
+
+insert into products (name, price, stock, category_id)
+values 	("Asus ROG i5 12400f", 1300, 12, 1),
+		("Razer hunstman mini", 130, 20, 3),
+		("Hp Probook i3 10100f", 600,10, 1);
+
+
 
